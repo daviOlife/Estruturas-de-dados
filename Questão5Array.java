8f58296a11fff587aa9b51c4fe7f458da9c1765e
Davi <davimjvj1235@gmail.com>
2025-09-20 18:16:42 -0300

    Update Questão5Array.java

--- Questão5Array.java
+++ Questão5Array.java
@@ -1,1 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class Questão5Array {
+    public static void main (String[] args) {
+        int[] numeros = new int[20];
+        List<Integer> pares = new ArrayList<>();
+        List<Integer> impares = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite os 20 números: ");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Número " + (i + 1));
+            numeros[i] = sc.nextInt();
+
+            }
+        
+            if (numeros[i] % 2 == 0) {
+                pares.add(numeros[i]);
+
+
+            }
+
+            else {
+                impares.add(numeros[i]);
+
+            }
+
+            System.out.println("Os números são: " + Arrays.toString(numeros));
+            System.out.println("Os números pares são: " + pares);
+            System.out.println("Os números ímpares são: " + impares);
+        }
+
+    }
