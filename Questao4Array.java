213d5886ae54c1fe930ae08db3a1171371f18149
Davi <davimjvj1235@gmail.com>
2025-10-07 17:34:38 -0300

    Add files via upload

--- Questao4Array.java
+++ Questao4Array.java
@@ -1,25 +1,13 @@
 import java.util.Scanner;
 
 public class Questao4Array {
-    public static void main (String[] args) {
-        char[] letras = new char[10];
+    public static void main(String[] args) {
+        String[] palavra = palavra[10];
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite os 10 caracteres: ");
-        for (int i = 0; i < letras.length; i++) {
-            System.out.println("Letra " + (i + 1));
-            letras[i] = sc.next().charAt(0);
-        }
-
-        System.out.println("Suas letras são: " + new String(letras)); // tem que transformar para string para leitura correta das letras
-
-            System.out.println("Suas letras somente com consoantes são: " );
-            for (char c : letras) {
-                if (Character.isLetter(c) && "aeiouAEIOU".indexOf(c) == -1) {
-                    System.out.println(c);
-                }
-            }
-sc.close();
-
+        for (i = 0; i < palavra.length; i++)
+            ;
+        System.out.println("Digite a palavra com 10 caracteres: " + (i + 1));
+        sc.nextInt(i);
     }
 }
